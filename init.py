50491c465aa2203b3c9b67327943a1c7ce103cb1
Caroline Frost <carolinefrost111@gmail.com>
2018-02-13 15:59:13 -0800

    fix init image,

--- init.py
+++ init.py
@@ -7,13 +7,16 @@ import matplotlib
 vgg_file = 'imagenet-vgg-verydeep-19.mat'
 vgg_model = scipy.io.loadmat(vgg_file)
 vgg_layers = vgg_model['layers']
+#print(vgg_layers)
 
+def import_model():
+    return vgg_model
 
 def get_weights(layer_num):
-    return vgg_layers[0][layer_num][0][0][0][0][0]
+    return vgg_layers[0][layer_num][0][0][2][0][0]
 
 def get_bias(layer_num):
-    return vgg_layers[0][layer_num][0][0][0][0][1]
+    return vgg_layers[0][layer_num][0][0][2][0][1]
 
 #Activation function from https://www.tensorflow.org/api_guides/python/nn#Activation_Functions
 def activ_func(feat):
@@ -21,14 +24,15 @@ def activ_func(feat):
 
 #Convolution function from https://www.tensorflow.org/api_guides/python/nn#Convolution
 #Takes in 1 layer from model to inform filter, 1 layer to act as input
-def conv_func(input_num, fil_num):
+def conv_func(input, fil_num):
     w = tf.constant(get_weights(fil_num))
-    b = tf.constant(get_bias(fil_num))
-    return tf.nn.conv2d(input_num, filter=w, strides=[1, 1, 1, 1], padding='SAME') + b
+    b = get_bias(fil_num)
+    b = tf.constant(np.reshape(b, (b.size)))
+    return tf.nn.conv2d(input, filter=w, strides=[1, 1, 1, 1], padding='SAME') + b
 
 #Pooling function from https://www.tensorflow.org/api_guides/python/nn#Pooling
-def pool():
-    return tf.nn.avg_pool(prev_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+def pool(prev):
+    return tf.nn.avg_pool(prev, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 #Network
 #Layer configuration information here: https://gist.githubusercontent.com/ksimonyan/3785162f95cd2d5fee77/raw/f02f8769e64494bcd3d7e97d5d747ac275825721/VGG_ILSVRC_19_layers_deploy.prototxt
@@ -36,39 +40,40 @@ def pool():
 #             [ repeat with each necessary vgg layer ]
 #            -> pool -> save in network
 #5 pools total
-def create_network(heigh, width, color_num):
+def create_network(height, width, color_num):
     network = {}
-    network['init'] = tf.Variable(np.zeros((1, height, width, color_num))*1.0)
+    network['init'] = tf.Variable(np.zeros((1, height, width, color_num)), dtype = 'float32')
+    #tf.cast(network['init'], tf.int32)
 
     #1
-    network['11'] = activ_func(conv_func(graph['init'], 0))
-    network['12'] = activ_func(conv_func(graph['11'], 2))
+    network['11'] = activ_func(conv_func(network['init'], 0))
+    network['12'] = activ_func(conv_func(network['11'], 2))
     network['p1'] = pool(network['12'])
 
     #2
-    network['21'] = activ_func(conv_func(graph['p1'], 5))
-    network['22'] = activ_func(conv_func(graph['21'], 7))
+    network['21'] = activ_func(conv_func(network['p1'], 5))
+    network['22'] = activ_func(conv_func(network['21'], 7))
     network['p2'] = pool(network['22'])
 
     #3
-    network['31'] = activ_func(conv_func(graph['p2'], 10))
-    network['32'] = activ_func(conv_func(graph['31'], 12))
-    network['33'] = activ_func(conv_func(graph['32'], 14))
-    network['34'] = activ_func(conv_func(graph['33'], 16))
+    network['31'] = activ_func(conv_func(network['p2'], 10))
+    network['32'] = activ_func(conv_func(network['31'], 12))
+    network['33'] = activ_func(conv_func(network['32'], 14))
+    network['34'] = activ_func(conv_func(network['33'], 16))
     network['p3'] = pool(network['34'])
     
     #4
-    network['41'] = activ_func(conv_func(graph['p3'], 19))
-    network['42'] = activ_func(conv_func(graph['41'], 21))
-    network['43'] = activ_func(conv_func(graph['42'], 23))
-    network['44'] = activ_func(conv_func(graph['43'], 25))
+    network['41'] = activ_func(conv_func(network['p3'], 19))
+    network['42'] = activ_func(conv_func(network['41'], 21))
+    network['43'] = activ_func(conv_func(network['42'], 23))
+    network['44'] = activ_func(conv_func(network['43'], 25))
     network['p4'] = pool(network['44'])
     
     #5
-    network['51'] = activ_func(conv_func(graph['p4'], 19))
-    network['52'] = activ_func(conv_func(graph['51'], 21))
-    network['53'] = activ_func(conv_func(graph['52'], 23))
-    network['54'] = activ_func(conv_func(graph['53'], 25))
+    network['51'] = activ_func(conv_func(network['p4'], 28))
+    network['52'] = activ_func(conv_func(network['51'], 30))
+    network['53'] = activ_func(conv_func(network['52'], 32))
+    network['54'] = activ_func(conv_func(network['53'], 34))
     network['p5'] = pool(network['54'])
     
     return network
--- train.py
+++ train.py
@@ -2,24 +2,28 @@ import numpy as np
 import scipy.io
 import tensorflow as tf
 import matplotlib
+import init
 
 #Filesystem organization
 videogame = 'image1'
 real = 'image1'
 created = 'created'
+IMAGE_WIDTH = 800
+IMAGE_HEIGHT = 600
 
 #Initialize constants
 #EXPERIMENT WITH THESE
-noise = .5
+noise = .5 #ratio
 beta = 5
 alpha = 100
-means = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
+means = np.array([123.68, 116.779, 103.939]).reshape((3,1))
 style_weights = [('11', 0.5), ('21', 1.0), ('31', 1.5),('41', 3.0),('51', 4.0)]
+ITERATIONS = 100
 
 #Content loss
 def L_content(sess, model):
     #i is content layer
-    i = 2
+    i = '42'
     #x is generated image
     #F is feature representation of x in given layer Li
     F = sess.run(model[i])
@@ -30,21 +34,72 @@ def L_content(sess, model):
     return loss
 
 #Style loss
-def L_style(sess, model, style_weights):
+def L_style(sess, model, weights):
     
     def layer_loss(out1, out2):
-        G = np.reshape(out1, (out1.shape[2] * out1.shape[1], out1.shape[3]))
-        A = np.reshape(out2, (out2.shape[2] * out2.shape[1], out2.shape[3]))
+        G = tf.reshape(out1, (out1.shape[2] * out1.shape[1], out1.shape[3]))
+        G = tf.matmul(tf.transpose(G), G)
+        A = tf.reshape(out2, (out1.shape[2] * out1.shape[1], out1.shape[3]))
+        A = tf.matmul(tf.transpose(A), A)
         squared_sumGA = tf.reduce_sum(tf.pow(G - A, 2))
-        return (1.0 / (4 * N**2 * M**2)) * squared_sumGA
+        return (1.0 / (4 * (out1.shape[2] * out1.shape[1])**2 * out1.shape[3]**2)) * squared_sumGA
     
     total_loss = 0.0
-    for layer, weight in style_weights:
-       temp_loss = layer_loss(sess.run(model[layer]), model[layer])
+    for layer, weight in weights:
+        temp_loss = layer_loss(sess.run(model[layer]), model[layer])
         total_loss += weight * temp_loss
 
     return total_loss
 
+#buggy
+def init_image(path):
+    img = scipy.misc.imread(path) * 1.0
+    reshape = np.reshape(img, ((1,) + img.shape))
+    return reshape
+    return np.reshape(img, ((1,) + img.shape)) - means
+
+def create_canvas(img):
+    noise_image = np.random.uniform(-20, 20,(1, img.shape[1], img.shape[2], img.shape[3])).astype('float32')
+    canvas = noise_image * noise + img * (1 - noise)
+    return canvas
+
 if __name__ == '__main__':
-    session = tf.session()
+    #initialize session, imagesx3
+    session = tf.Session()
+    content = init_image('content/test.jpg')
+    style = init_image('style/test.jpg')
+    vgg = init.import_model()
+    
+    model = init.create_network(IMAGE_HEIGHT, IMAGE_WIDTH, 3)
+    canvas = create_canvas(content)
+    session.run(tf.initialize_all_variables())
+    
+    #content loss from content image
+    session.run(model['init'].assign(content))
+    L_content = L_content(session, model)
+
+    #style loss from style image
+    session.run(model['init'].assign(style))
+    L_style = L_style(session, model, style_weights)
+
+
+    #there is a tradeoff between mainting content and introducing stlye, therefore
+    #the loss function to minimize: Ltotal(~p,~a, ~x) = alpha * Lcontent(p, x) + beta * Lstyle(a, x)
+    total_loss = alpha * L_content + beta * L_style
+
+    #train
+    optimizer = tf.train.AdamOptimizer(2.0)
+    train_step = optimizer.minimize(total_loss)
+
+    session.run(model['init'].assign(canvas))
+
+    for it in range(ITERATIONS):
+        session.run(train_step)
+        if it%100 == 0:
+            # Print every 100 iteration.
+            mixed_image = session.run(model['init'])
+            print('Iteration %d' % (it))
+            print('sum : ', session.run(tf.reduce_sum(mixed_image)))
+            print('cost: ', session.run(total_loss))
+
 
